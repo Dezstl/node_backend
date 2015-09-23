7b71f6e392c7e0b0103b3a89beed7e3e7a7bb534
Dezstl <dezstl86@yahoo.com>
2015-09-23 00:13:47 -0500

    add bcrypt

--- app/models/User.js
+++ app/models/User.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+
 var UserSchema = new Schema({
 		firstName: { type: String, required: true },
     	lastName: { type: String, required: true },
@@ -10,6 +13,17 @@ var UserSchema = new Schema({
         state: { type: String, required: true},
         active: {type: Boolean, required: true},
         password: {type: String, required: true}
+});
+
+var UserSchema.pre('save', function (next){
+	var user = this;
+
+	// Check if password was changed
+	if (!user.isModified('password')) {
+		return next();
+	}
+
+
 });
 
 module.exports = mongoose.model('User', UserSchema);
