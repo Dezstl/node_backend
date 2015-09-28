3470096fe5e60b61fe02b4ccc1aa7e5072626fa9
Dezmond Moore <Dezmond.moore@yahoo.com>
2015-09-27 20:51:10 -0500

    Refactor to allow versioning

---
+++ app/routes/v1.js
@@ -0,0 +1,26 @@
+//Version 1
+var express = require('express');
+var session = require('express-session');
+var app = module.exports = express();
+var guid = require('node-uuid');
+var passport = require('./v1/modules/auth/passport')();
+
+//Setup passport
+app.use(session({
+  genid: function(req) {
+    return guid.v4() // use UUIDs for session IDs
+  },
+  secret: 'proto x',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Middleware to ensure user is authenticated
+app.use(require('./v1/middleware/ensureAuthenticated').ensureAuthenticated);
+
+require('./v1/controllers/index')(app);
+require('./v1/controllers/user')(app);
+require('./v1/controllers/auth')(app);
+require('./v1/controllers/environment')(app);
--- app/routes/v1/controllers/index.js
+++ app/routes/v1/controllers/index.js
@@ -1,11 +1,11 @@
 module.exports = function(app) {
 
 	app.get('/', function (req, res) {
-		res.redirect('/api');
+		res.redirect('/v1/api');
 	});
 
 	app.get('/api', function (req, res) {
-		res.json({message: "API IS UP"});
+		res.json({message: "API IS UP, Version 1"});
 	});
 
 };
